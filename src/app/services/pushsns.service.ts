e60fdb8406b932d3756e6dd768a92c7e68189acb
Jak Ratiwanich <jratiwanich@idemia-nss.com>
2019-08-19 20:41:17 -0700

    add plugin

--- src/app/services/pushsns.service.ts
+++ src/app/services/pushsns.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
 //import {PushsnsPluginWeb } from 'pushsns-plugin';
+import { Plugins } from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PushsnsService {
 
-  constructor(){}
+  private pushsns;
+
+  constructor(){
+    this.pushsns =   Plugins.PushsnsPlugin;
+  }
 
   //constructor(private pushweb: PushsnsPluginWeb) { }
 
   init(tokenAPNS){
+    if(this.platform.is('ios')){
+      ///
+    }else{
+////
+    }
     //{registrationId: "18621b25fc1506c6ca0791ab42a5153e612b0b77ed483a60fb9b2b35a5d229a2", registrationType: "APNS"} (cordova.js, line 1509)
     // this.pushweb.initSns({
     //   token: tokenAPNS,
